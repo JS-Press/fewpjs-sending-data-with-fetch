5e3ceb6417f8336a4496581c417cd6cd89ead6f6
Jarad Solomon <jaradsolomon@gmail.com>
2021-06-13 23:30:42 -0500

    commit tree 9bc8a4288a5b381cd55d6cc6cce675e8bc27e9cd

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Add your code here
+
+// function fetchy(){
+// fetch('http://localhost:3000/dogs')
+// .then (r => r.json())
+// .then (j => console.log(j));
+// }
+
+
+function submitData(userName, userEmail){
+
+    const formData = {
+        'name': userName,
+        'email' : userEmail
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(data => document.body.innerHTML = data.id)
+        .catch(obj => document.body.innerHTML = obj.message)
+
+
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => 
+   submitData()
+)
